66edd79b25f5286ad6799f3d7dca1e510455ba4f
mfujimot-1125 <qpmonqp1111@gmail.com>
2025-03-06 17:01:42 +0900

    Update research_dic_prediction_2025_04_labeling_dic_diagnosis.sql

--- sql/research_dic_prediction_2025_04_labeling_dic_diagnosis.sql
+++ sql/research_dic_prediction_2025_04_labeling_dic_diagnosis.sql
@@ -1,43 +1,24 @@
 with
+    first_dic as (
+      select
+        icu_stay_id,
+        timestamp_trunc(min(start_time), day) as first_dic_time,
+        1 as label_dic_diagnosis
+      from {{ ref("medicu", "one_icu_derived_dic_hourly") }}
+      where isth_dic_24hours >= 5 and time_window_index >= 0
+      group by icu_stay_id
+    ),
     dic_score_joined as (
-        select
+         select
             tw.icu_stay_id,
-            tw.in_time,
-            tw.out_time,
             tw.time_window_index,
             tw.start_time,
             tw.end_time,
-            dic.time_window_index as dic_time_window_index,
-            dic.isth_platelet_count_score,
-            dic.isth_pt_score,
-            dic.isth_fibrinogen_score,
-            dic.isth_d_dimer_score
-        from {{ ref("medicu", "research_dic_prediction_2025_02_time_windows") }} tw
-        left join
-            {{ ref("medicu", "one_icu_derived_dic_hourly") }} dic
-            on tw.icu_stay_id = dic.icu_stay_id
-            and dic.start_time >= tw.end_time
-            and datetime_add(tw.end_time, interval 1 day) >= dic.end_time
-            and dic.isth_platelet_count_score is not null
-    ),
-    labeling_dic_diagnosis as (
-        select
-            icu_stay_id,
-            time_window_index,
-            start_time,
-            end_time,
-            case
-                when
-                    min_by(ifnull(isth_platelet_count_score, 0), dic_time_window_index)
-                    + min_by(ifnull(isth_pt_score, 0), dic_time_window_index)
-                    + min_by(ifnull(isth_fibrinogen_score, 0), dic_time_window_index)
-                    + min_by(ifnull(isth_d_dimer_score, 0), dic_time_window_index)
-                    >= 5
-                then 1
-                else 0
-            end as label_dic_diagnosis
-        from dic_score_joined
-        group by icu_stay_id, time_window_index, start_time, end_time
+            ifnull(label_dic_diagnosis, 0) as label_dic_diagnosis
+            from {{ ref("medicu", "research_dic_prediction_2025_02_time_windows") }} tw
+            left join first_dic
+            on tw.icu_stay_id = first_dic.icu_stay_id 
+            and tw.end_time = first_dic.first_dic_time
     )
 select *
-from labeling_dic_diagnosis
+from dic_score_joined
